a65160c2cf91ebbd958e9cc0e1cd132054831060
purna21 <mepurnachandra21@gmail.com>
2023-05-09 19:16:47 +0530

    JS Variables Reference

---
+++ Variables/variables.js
@@ -0,0 +1,35 @@
+// *** What are Variables? *** //
+
+/* 
+Variables are a way for us to store different types of values in them.
+They are like a container that holds values.
+
+For a simple analogy, we put laptop in a bag. Here, bag is a Variable and
+laptop is value.
+*/
+
+
+// *** Let's look at some code *** //
+
+let name = 'Purna';
+const age = 25;
+let isMajor = true;
+
+/* 
+See in the above code, forget about the words like let and const. We will
+discuss about them later. 
+
+Just to clear, let and const are different ways how we declare a Variable.
+
+Here name is the Variable name and 'Purna' is the value we stored in it.
+Same goes for age and isMajor Variable. Left side we have Variable names and
+Right side we store corresponding values.
+*/
+
+/*
+We have console.log() method which shows the output in the Browser console.
+*/
+
+console.log(name);
+console.log(age);
+console.log(isMajor);
